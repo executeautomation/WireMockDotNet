9d46eb71c69d0e7561d49fba2fa164c3e639ed69
ExecuteAutomation <kartmcad@gmail.com>
2024-05-14 14:24:23 +1200

    Reformatted the code

--- WireMockNetServer/Program.cs
+++ WireMockNetServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using WireMock.Matchers;
+﻿using System.Net;
+using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
@@ -6,13 +7,14 @@ using WireMock.Settings;
 
 
 // Server for Wiremock.NET
-var server =WireMockServer.Start(new WireMockServerSettings
+var server = WireMockServer.Start(new WireMockServerSettings
 {
-    Urls = [
+    Urls =
+    [
         "http://localhost:8080"
     ],
     StartAdminInterface = true,
-    ReadStaticMappings = true,
+    ReadStaticMappings = true
 });
 
 
@@ -30,131 +32,129 @@ Console.WriteLine("Started WireMock.NET server");
 //Stub - Test
 server.Given(Request.Create().WithPath("/test").UsingGet())
     .RespondWith(Response.Create().WithBody("Welcome to Wiremock .NET Test")
-    .WithHeader("Content-Type", "application/json"));
+        .WithHeader("Content-Type", "application/json"));
 
 //Stub - Header
 server.Given(Request.Create().WithPath("/headers").UsingGet())
     .RespondWith(Response.Create().WithBody("Welcome to Wiremock .NET Test for headers")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 
 server.Given(Request.Create().WithPath("/headers").UsingGet())
     .RespondWith(Response.Create().WithBody("Headers with seconds stub call")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
-
 
 
 server.Given(Request.Create().WithPath("/headers").UsingPut())
     .RespondWith(Response.Create().WithBody("Welcome to Wiremock .NET Test for headers")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 //Stub - ProductId
 server.Given(Request.Create().WithPath(new RegexMatcher("/product/[0-9]+$")).UsingGet())
     .RespondWith(Response.Create().WithBody("Keyboard is the product")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 //Stub - ProductId
 server.Given(Request.Create().WithPath(new WildcardMatcher("/product/*")).UsingGet())
     .RespondWith(Response.Create().WithBody("This could be any product")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 //Stub - login
 server.Given(Request
-    .Create()
-    .WithPath("/login")
-    .UsingGet()
-    .WithHeader("Authorization", new WildcardMatcher("Bearer *")))
+        .Create()
+        .WithPath("/login")
+        .UsingGet()
+        .WithHeader("Authorization", new WildcardMatcher("Bearer *")))
     .RespondWith(Response.Create().WithBody("Login Successful")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 //Stub - login
 server.Given(Request
-    .Create()
-    .WithPath("/login")
-    .UsingGet()
-    .WithHeader("Authorization", "*", MatchBehaviour.RejectOnMatch))
+        .Create()
+        .WithPath("/login")
+        .UsingGet()
+        .WithHeader("Authorization", "*", MatchBehaviour.RejectOnMatch))
     .RespondWith(Response.Create().WithBody("Login UnSuccessful")
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Unauthorized)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Unauthorized)
     );
-
 
 
 //Stub - GetAddress
 server.Given(Request.Create().WithPath(new WildcardMatcher("/getAddress/*")).UsingGet())
     .RespondWith(Response.Create().WithBodyAsJson(new
-    {
-        Name = "Karthik",
-        City = "Auckland",
-        Country = "NZ"
-    })
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        {
+            Name = "Karthik",
+            City = "Auckland",
+            Country = "NZ"
+        })
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 
 //Stub - GetAddress
 server.Given(Request.Create().WithPath(new WildcardMatcher("/getAddressFromObject/*")).UsingGet())
     .RespondWith(Response.Create().WithBodyAsJson(new Address("Prashanth", "Chennai", "India"))
-    .WithHeaders(new Dictionary<string, string>
-    {
-        {"Content-Type", "application/json"},
-        {"Accept", "application/json"},
-        {"Cache-Control", "no-cache" }
-    })
-    .WithStatusCode(System.Net.HttpStatusCode.Accepted)
+        .WithHeaders(new Dictionary<string, string>
+        {
+            { "Content-Type", "application/json" },
+            { "Accept", "application/json" },
+            { "Cache-Control", "no-cache" }
+        })
+        .WithStatusCode(HttpStatusCode.Accepted)
     );
 
 
